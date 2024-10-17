d42bf62a2afb58fdddd3c041a811315c85c8b3d8
rushabhdharia <rushabh.dharia@gmail.com>
2024-10-17 01:40:19 -0400

    Add project files.

---
+++ AI Voyage Concierge/Controllers/AIController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AI_Voyage_Concierge.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AIController : ControllerBase
+    {
+        [HttpPost(Name = "GetTravelItenary")]
+        public string GetTravelItenary()
+        {
+            /*
+             * 1. Different Locations
+             * 2. Number of Days
+             * 3. Freeform text for explaination
+             */
+            return "";
+        }
+
+        [HttpPost(Name = "GetInformationAboutLocation")]
+        public string GetInformationAboutLocation()
+        {
+            /*
+             * 1. Ask Questions to Bot
+             */
+            return "";
+        }
+
+        [HttpPost(Name = "LocationBasedNotifications")]
+        public string LocationBasedNotifications()
+        {
+            /*
+             * 1. Get Location and get notifications
+             */
+            return "";
+        }
+
+
+
+    }
+}
