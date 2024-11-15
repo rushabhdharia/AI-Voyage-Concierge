68796dcaf6215bb833b9a83c8aefd405c17825f2
rushabhdharia <rushabh.dharia@gmail.com>
2024-11-14 16:03:01 -0800

    Added JWT token creation

--- AI Voyage Concierge/Controllers/IdentityController.cs
+++ AI Voyage Concierge/Controllers/IdentityController.cs
@@ -1,6 +1,11 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using AI_Voyage_Concierge.Entities;
 using AI_Voyage_Concierge.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using MongoDB.Driver;
 
 namespace AI_Voyage_Concierge.Controllers;
@@ -10,14 +15,53 @@ namespace AI_Voyage_Concierge.Controllers;
 public class IdentityController : Controller
 {
     private readonly IMongoCollection<User> _users;
+    private readonly IConfiguration _configuration;
     // Constructor
-    public IdentityController(MongoDbService mongoDbService)
+    public IdentityController(MongoDbService mongoDbService, IConfiguration configuration)
     {
         _users = mongoDbService.Database.GetCollection<User>("users");
+        _configuration = configuration;
     }
     [HttpGet]
     public async Task<IEnumerable<User>> Get()
     {
         return await _users.Find(_ => true).ToListAsync();
     }
-}
+    
+    [HttpPost]
+    public async Task<User> Create(User user)
+    {
+        await _users.InsertOneAsync(user);
+        return user;
+    }
+    
+    [HttpGet]
+    public async Task<User> GetUserByEmail(string email)
+    {
+        return await _users.Find(x => x.Email == email).FirstOrDefaultAsync();
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Authenticate (User user)
+    {
+        var currentUser = await _users.Find(x => x.Email == user.Email && x.Password == user.Password).FirstOrDefaultAsync();
+        if (currentUser == null) throw new Exception("User not found");
+        
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_configuration.GetSection("JwtKey").Value ?? throw new InvalidOperationException());
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, currentUser.Email ?? throw new InvalidOperationException())
+            }),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        var tokenString = tokenHandler.WriteToken(token);
+        
+        return Ok(tokenString);
+
+    }
+} 
--- AI Voyage Concierge/Entities/User.cs
+++ AI Voyage Concierge/Entities/User.cs
@@ -7,17 +7,23 @@ public class User
 {
     [BsonId]
     [BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
-    public required string Id { get; set; }
+    public string? Id { get; set; }
     
     [BsonElement("name"), BsonRepresentation(BsonType.String)]
-    public required string Name { get; set; }
+    public string? Name { get; set; }
 
     [BsonElement("email"), BsonRepresentation(BsonType.String)]
-    public required string Email { get; set; }
+    public string? Email { get; set; }
     
-    [BsonElement("password_salt"), BsonRepresentation(BsonType.String)]
-    public required string PasswordSalt { get; set; }
+    //refactor once salt and hash are implemented
+    [BsonElement("password"), BsonRepresentation(BsonType.String)]
+    public string? Password { get; set; }
     
-    [BsonElement("password_hash"), BsonRepresentation(BsonType.String)]
-    public required string PasswordHash { get; set; }
+    //implement later
+    // [BsonElement("password_salt"), BsonRepresentation(BsonType.String)]
+    // public required string PasswordSalt { get; set; }
+    
+    //implement later
+    // [BsonElement("password_hash"), BsonRepresentation(BsonType.String)]
+    // public required string PasswordHash { get; set; }
 }
